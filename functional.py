610eae41619821cbe961366ab69506cb795364b8
Swetychekkk <stepankursk1@gmail.com>
2024-12-10 15:40:34 +0300

    added auto config creation on app initialisation

---
+++ functional.py
@@ -0,0 +1,56 @@
+import shutil
+import random
+import configparser
+import os
+
+list_of_files = []
+path_to_rust = "none"
+
+config = configparser.ConfigParser()
+def init_functions():
+    global path_to_rust, debug
+    config.read("config.ini")
+
+    #values
+    path_to_rust = config["MAIN_CONFIG"]["RUST_PATH"]
+    debug = config["MAIN_CONFIG"]["DEBUG"]
+
+#logger
+def log(text):
+    if debug == True:
+        print("LOG: " + text)
+
+#change path to rust
+def change_path_to_rust(path):
+    global path_to_rust
+    if "RustClient.exe" in os.listdir(path):
+        config["MAIN_CONFIG"]["RUST_PATH"] = f"{path}/temp"
+        with open('config.INI', 'w+') as configfile:
+            config.write(configfile)
+        path_to_rust = config["MAIN_CONFIG"]["RUST_PATH"]
+        log("path_changed_to " + path + "/temp")
+        return True
+    else:
+        return False
+
+#initialisating list of loading screen images
+def init_files():
+    global list_of_files
+    for i in os.listdir(path_to_rust):
+        if ".jpg" in i:
+            log(i + " ADDED TO LIST")
+            if not i in list_of_files:
+                list_of_files.append(i)
+
+#replacing pictures (multiple)
+def replace(file_list_to_copy):
+    if len(file_list_to_copy) == len(list_of_files):    #if number of pictures in folder equals number of pictures in game
+        for i in len(list_of_files):
+            os.remove(f"{path_to_rust}/{list_of_files[i]}")
+            shutil.copy(file_list_to_copy[i], f"{path_to_rust}/{list_of_files[i]}")
+            log(i + " REPLACED")
+    else:   #else select random pictures to replace
+        for i in list_of_files:
+            os.remove(f"{path_to_rust}/{i}")
+            shutil.copy(f"{random.choice(file_list_to_copy)}", f"{path_to_rust}/{i}")
+            log(i + " REPLACED USING RANDOM")
---
+++ main.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from tkinter import ttk, filedialog, messagebox
+from functional import *
+
+
+#root init
+root = Tk()
+root.title("R A L S")
+root.geometry("310x100")
+
+root["bg"] = "#393f45"
+
+#basic functions
+def ask_for_path():
+    filedial = filedialog.askdirectory()
+    if change_path_to_rust(filedial):
+        messagebox.showinfo("SUCCESS", "RUST PATH IS CORRECT")
+        init_files()
+        label_count.config(text=f"Total files: {len(list_of_files)}")
+    else:
+        messagebox.showerror("ERROR", "Please, select valid path")
+
+def ask_for():
+    filedial = filedialog.askopenfilenames(filetypes=[(f"To fill: {len(list_of_files)}", ".jpg")])
+    if len(filedial) > 0:
+        replace(filedial)
+        messagebox.showinfo("SUCCESS", "Done")
+    else:
+        messagebox.showwarning("WARNING", "Error handler: select more then one file")
+
+style = ttk.Style()
+style.configure("TLabel", foreground="white", background="#393f45")
+style.configure("TButton", foreground="black", background="#393f45")
+
+#init ui elements
+frame = Frame(root)
+frame.config(bg="#393f45")
+frame.pack()
+btn_change_folder = ttk.Button(frame, text="CHANGE RUST PATH", command=ask_for_path, width=30, style="TButton")
+label_count, label_subtitle = ttk.Label(frame, text="n/a", style="TLabel",width=50), ttk.Label(text="RustAutoLoadingScreen(pb1) by Swetychek", style="TLabel")
+btn_select = ttk.Button(text="Select files", command=ask_for, width=60, style="TButton")
+
+def ui_pack():
+    #pack ui elements
+    btn_change_folder.pack(side=LEFT, pady=10, padx=15)
+    label_count.pack(side=LEFT, padx=0)
+    btn_select.pack(anchor=W, padx=15)
+    label_subtitle.pack(anchor=W, padx=15, pady=5)
+
+
+def initialisating_files():
+    global label_count, path_to_rust
+    #init all files
+    if path_to_rust != "none":
+        print("sadasdsad")
+        init_files()
+        label_count.config(text=f"Total files: {len(list_of_files)}")
+
+if __name__ == "__main__":
+    if not "config.ini" in os.listdir():
+        config_file = open("config.ini", "w")
+        config_file.write("[MAIN_CONFIG]\nrust_path = NONE\ndebug = none")
+        config_file.close()
+    init_functions()
+    initialisating_files()
+    ui_pack()
+
+root.mainloop()
